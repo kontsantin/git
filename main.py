df50be9543614ea23f0b4f03d70ec84005a175c6
Kontsantin <mikhajlov89@mail.ru>
2025-06-19 19:00:32 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://drom.ru/'
+utf8_header = {'Content-Type': 'text/html; charset=utf-8'}
+def parse_title():
+    response = requests.get(url, headers=utf8_header)
+    if response.status_code == 200:
+        return response.text.split('<title>')[1].split('</title>')[0]
+    else:
+        return None
+
+print(parse_title())
